2f06ae901b21190ea8d6bd48b6fe773e975335f0
rgcoding <raviraj.kumar@live.com>
2023-06-27 12:07:35 -0500

    Updated with comments

--- hob-labels-etl/lambda_function.py
+++ hob-labels-etl/lambda_function.py
@@ -1,13 +1,19 @@
+
+#
+# Program - Process HOB Label data and load into SQL Server for reporting
+# Authon - Raviraj Gubbala
+#
 import subprocess
 import os
-from cresco_etl_helpers import CrescoLambdaClient
+from havi_etl_helpers import HAVILambdaClient
 from postgres import Postgres
 import json
 import boto3
 import botocore
 import pandas as pd
-import snow
-import mssqlserver
+from database import snow
+from database import mssqlserver
+from hobsql import hobsql
 import email_util
 from io import BytesIO
 from datetime import datetime, timedelta
@@ -20,13 +26,14 @@ current_dt = datetime.now().date()
 start_dt = (current_dt - timedelta(30)).strftime('%Y-%m-%d')
 havi_lambda_client = HAVILambdaClient()
 
+#SNOW Flake connection information
 snowflake = snow.Snowflake(WAREHOUSE='COMPUTE_WH', DATABASE='DEMO_DB', SCHEMA='ETL', ROLE='ETL_LOAD_ROLE')
 sql_server_conn = json.loads(os.environ['SQL_SERVER'])
 sql_server = mssqlserver.MSSqlServer(host=sql_server_conn['host'], user=sql_server_conn['user'],
                                      password=sql_server_conn['password'], port=sql_server_conn['port'],
                                      database=sql_server_conn['database'], driver=sql_server_conn['driver'])
 
-
+#Formatting columns to standard
 def format_columns(val):
     known_value = get_known_label(val)
     if known_value:
@@ -45,7 +52,7 @@ def format_columns(val):
             frags[x] = frags[x][0].upper() + frags[x][1:]
     return ' '.join(frags)
 
-
+#Look up for column mapping values
 def get_known_label(argument):
     switcher = {
         'cbd_1_to_1': 'CBD 1:1',
@@ -75,7 +82,7 @@ def lambda_handler(event, context):
             file_path = '/tmp/' + file
             if not os.path.exists(file_path):
                 s3 = boto3.resource('s3')
-                bucket_name = 'data-eng-cresco-labs'
+                bucket_name = 'data-eng-havi-labs'
                 key = 'keys/' + file
                 try:
                     local_file_name = '/tmp/' + file
@@ -85,38 +92,11 @@ def lambda_handler(event, context):
                 except botocore.exceptions.ClientError as e:
                     raise RuntimeError(e)
         df_list = []
+        #Looping through the different databases
         for database in databases:
             connection_details = os.environ[database]
             connection_details_dict = json.loads(connection_details)
-            sql = """SELECT i.id AS inventory_id,Btrim(i.strain) AS inventory_strain,
-CASE
- WHEN p.name IS NULL THEN Btrim(i.strain)
- ELSE Btrim(p.name)
-END As product_name,
-pc.name as product_category,
-p.strain AS bt_product_strain,
-Date(i.created) as created,
-CASE
-    WHEN (i.inventorytype = -1) THEN 'Accessories'
-    ELSE it.name
-END AS inventory_type,
-l.name AS location_name,
-to_char(to_timestamp(s.sessiontime),'YYYY-MM-DD') AS date_tested,
-i.sample_id AS sample_id,
- CASE WHEN s.result = 1 THEN 'Passed' WHEN s.result = -1 THEN 'Failed' ELSE 'Pending' END AS sample_status,cbd AS 
- bt_potency_cbd, thca AS bt_potency_thca, thc AS bt_potency_thc, cbdA AS bt_potency_cbda,qa_total AS bt_potency_total, 
- custom_1 AS Terpenes FROM inventory i JOIN (SELECT x.inventoryparentid,(qa_data-> 'CBD')::numeric AS cbd,
- (qa_data-> 'CBDA')::numeric AS cbda,(qa_data-> 'THC')::numeric AS thc,
- (qa_data-> 'THCA')::numeric AS thca,qa_data-> 'Total' AS qa_total,qa_data,y.sessiontime,y.inventoryid,
- y.result FROM (SELECT sa.inventoryparentid,sa.id,string_agg(CONCAT_WS('=>',r.name,r.value),',')::hstore as qa_data 
- FROM (SELECT MAX(id) AS id,inventoryparentid FROM bmsi_labresults_samples WHERE deleted = 0 GROUP BY 
- inventoryparentid) sa JOIN bmsi_labresults_potency_analysis r ON r.sample_id = sa.id GROUP BY 
- sa.inventoryparentid,sa.id) x JOIN bmsi_labresults_samples y ON y.id = x.id) s ON 
- s.inventoryparentid = i.inventoryparentid LEFT JOIN products p ON i.productid = p.id LEFT JOIN inventoryrooms ir 
- ON ir.id = i.currentroom INNER JOIN locations l ON l.id = i.location LEFT JOIN inventorytypes it 
- ON it.id = i.inventorytype LEFT JOIN productcategories pc ON p.productcategory = pc.id LEFT JOIN vendors v ON 
- p.defaultvendor = v.id  where i.created between '{start_date}' and '{current_date}' """\
-                .format(start_date=start_dt, current_date=current_dt)
+            sql = hobsql.sql_lab_results.format(start_date=start_dt, current_date=current_dt)
 
             postgres = Postgres(host=connection_details_dict['host'], user=connection_details_dict['user'],
                                 password=connection_details_dict['password'], port=connection_details_dict['port'],
@@ -124,11 +104,12 @@ i.sample_id AS sample_id,
                                 ssl_cert='/tmp/postgresql.crt', ssl_key='/tmp/postgresql.key')
             product_inventory_df = postgres.read_sql_as_pandas_df(sql)
             if not product_inventory_df.empty:
+                #Getting terpenese values
                 del product_inventory_df['terpenes']
                 product_inventory_cat_req_df = product_inventory_df[["inventory_id", "product_name", "product_category",
                                                                  "inventory_type", "inventory_strain"]]
                 product_list = product_inventory_cat_req_df.to_dict(orient='records')
-                cat_df = pd.DataFrame(cresco_lambda_client.get_categorization(product_list))
+                cat_df = pd.DataFrame(havi_lambda_client.get_categorization(product_list))
                 if cat_df.empty:
                     continue
                 product_category_df = pd.merge(product_inventory_df, cat_df, how='inner', left_on='inventory_id',
@@ -139,6 +120,7 @@ i.sample_id AS sample_id,
         appended_product_category_df = pd.concat(df_list)
         if not appended_product_category_df.empty:
             buffer = BytesIO()
+            #Transforming the column values
             appended_product_category_df['BT_INVENTORY_ID'] = appended_product_category_df['BT_INVENTORY_ID']. \
                 apply(lambda x: int(x))
             appended_product_category_df['INVENTORY_ID'] = appended_product_category_df['INVENTORY_ID']. \
@@ -161,15 +143,7 @@ i.sample_id AS sample_id,
                     appended_product_category_df[col].apply(lambda x: x.title() if pd.notnull(x) else x)
 
             appended_product_category_df = appended_product_category_df[
-                ['BRAND', 'BRAND_CODE', 'BT_INVENTORY_ID', 'BT_INVENTORY_STRAIN',
-                 'CATEGORY', 'CBD_THC_RATIO', 'COUNT', 'CULTIVATOR', 'EXTRACTION_METHOD',
-                 'FLAVOR', 'NAME', 'PRODUCT_CODE', 'PRODUCT_ID', 'PRODUCT_STRAIN_ID',
-                 'STRAIN_CODE', 'STRAIN_TYPE', 'SUB_CATEGORY', 'SUB_CATEGORY_01',
-                 'TERPENES', 'WEIGHT', 'WEIGHT_EACH', 'WEIGHT_UNIT',
-                 'WEIGHT_VALUE', 'INVENTORY_ID', 'PRODUCT_NAME', 'PRODUCT_CATEGORY',
-                 'INVENTORY_STRAIN', 'INVENTORY_TYPE', 'BT_POTENCY_CBD',
-                 'BT_POTENCY_THCA', 'BT_POTENCY_THC', 'BT_POTENCY_CBDA',
-                 'BT_POTENCY_TOTAL', 'LOAD_DT']]
+                hobsql.col_prod_category]
             sql_server.write_pandas_df(appended_product_category_df, 'joliet_il', 'dbo', if_exists='append')
             writer = pd.ExcelWriter('temp.xlsx', engine='xlsxwriter')
             writer.book.filename = buffer
